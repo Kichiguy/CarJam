2f78ecc3ad0112636863c29c2aaa104df07bc803
Kichiguy <sedarmor@gmail.com>
2021-06-14 07:51:26 -0500

    Added pause menu and pause state. Added quit game and resume features.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PauseMenu menu;
+    public bool paused;
+    void Start()
+    {
+        paused = false;
+    }
+
+    void Update()
+    {
+        //Pause button
+        if(Input.GetKeyDown(KeyCode.F)) {
+            paused = true;
+            menu.gameObject.SetActive(true);
+        }
+    }
+}
