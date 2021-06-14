732fb700aae63d1eb2ad970a107950fa9f8ff474
Bobby Brace <bob.j.brace@gmail.com>
2021-06-14 12:52:25 -0500

    road moves

---
+++ Assets/Scripts/MoveRoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRoad : MonoBehaviour
+{
+    public float speed;
+    public PlayerController player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(!player.paused) {
+            //TODO: change this to allow for directional changes
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+    }
+}
