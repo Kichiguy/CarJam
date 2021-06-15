9630f7705c52076d91b264b8e20fa5a94ad5d6f7
Kichiguy <sedarmor@gmail.com>
2021-06-15 07:16:20 -0500

    Fixed spawning and destruction issues. Roads spawn at game start.

--- Assets/Scripts/MoveRoad.cs
+++ Assets/Scripts/MoveRoad.cs
@@ -6,10 +6,12 @@ public class MoveRoad : MonoBehaviour
 {
     private PlayerController player;
     private RoadSpawner spawner;
+    private double destroyPoint;
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         spawner = GameObject.FindGameObjectWithTag("GameController").GetComponent<RoadSpawner>();
+        destroyPoint = GetComponent<BoxCollider>().size.z * -1.5;
     }
 
     void Update()
@@ -17,6 +19,10 @@ public class MoveRoad : MonoBehaviour
         if(!player.paused) {
             //TODO: change this to allow for directional changes
             transform.Translate(Vector3.back * Time.deltaTime * spawner.roadSpeed);
+            
+            if(transform.position.z < destroyPoint) {
+                Destroy(gameObject);
+            }
         }
     }
 }
--- Assets/Scripts/RoadSpawner.cs
+++ Assets/Scripts/RoadSpawner.cs
@@ -4,42 +4,27 @@ using UnityEngine;
 
 public class RoadSpawner : MonoBehaviour
 {
-    public MoveRoad currentRoad;
     public GameObject straightRoad;
+    public PlayerController player;
     public float roadSpeed;
-
-    private Vector3 startPos;
-    private Vector3 spawnPos;
-    private float repeatWidth;
-    private MoveRoad oldRoad;
     void Start()
     {
-        startPos = currentRoad.transform.position;
-        repeatWidth = currentRoad.GetComponent<BoxCollider>().size.z / 2;
-        spawnPos = new Vector3(startPos.x, startPos.y, startPos.z + repeatWidth);
+        Instantiate(straightRoad, new Vector3(7, 0, 0), straightRoad.transform.rotation);
+        Instantiate(straightRoad, new Vector3(7, 0, 130), straightRoad.transform.rotation);
+        Instantiate(straightRoad, new Vector3(7, 0, 260), straightRoad.transform.rotation);
+        Instantiate(straightRoad, new Vector3(7, 0, 390), straightRoad.transform.rotation);
     }
 
     void Update()
     {
-        if(currentRoad.transform.position.z < startPos.z - repeatWidth) {
+        if(!player.paused) {
+            if(GameObject.FindGameObjectsWithTag("Road").Length < 4) {
             SpawnRoad();
-        }
-        if(currentRoad.transform.position.z < -currentRoad.GetComponent<BoxCollider>().size.z) {
-            SetNewCurrentRoad();
-            DestroyOldRoad();
+            }
         }
     }
 
     void SpawnRoad() {
-        Instantiate(straightRoad, spawnPos, currentRoad.transform.rotation);
-    }
-
-    void DestroyOldRoad() {
-        Destroy(oldRoad.gameObject);
-    }
-
-    void SetNewCurrentRoad() {
-        oldRoad = currentRoad;
-        currentRoad = GameObject.FindGameObjectsWithTag("Road")[1].GetComponent<MoveRoad>();
+        Instantiate(straightRoad, transform.position, straightRoad.transform.rotation);
     }
 }
